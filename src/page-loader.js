fd515581ae1696d081b3539ccf3b76f6eb34e56c
iskaldvind <iskaldvind@gmail.com>
2017-06-19 17:31:04 +0300

    still a tons of bugs

--- src/page-loader.js
+++ src/page-loader.js
@@ -2,15 +2,7 @@ import fs from 'mz/fs';
 import axios from './lib/axios';
 import resolvePath from './lib/helpers';
 
-/*
 export default (address, dir) => axios.get(address)
   .then((response) => {
     fs.writeFile(resolvePath(address, dir), response.data).catch(error => error);
   });
-*/
-
-export default (address, dir) => {
-  return axios.get(address)
-    .then(response => fs.writeFile(resolvePath(address, dir), response.data))
-    .catch(err => err);
-};
